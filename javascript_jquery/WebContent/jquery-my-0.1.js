ff657348667ff5300c26a1fa37b0d8f5b5d4aa41
haewoni <suzyhw96@gmail.com>
2020-10-19 12:54:45 +0900

    xml dom jquery

---
+++ javascript_jquery/WebContent/jquery-my-0.1.js
@@ -0,0 +1,30 @@
+window.jQuery=function(selector){
+	if(typeof selector=='string'){
+		
+	}else if(typeof selector=='function'){
+		
+	}else if(typeof selector=='object'){
+	
+	}
+	var elementNodeList = document.querySelectorAll(selector);
+	var elementNode = document.querySelector(selector);
+	var jQueryWrapperObject={
+		'elementNodeList':elementNodeList,
+		'elementNode':elementNode,
+		'css':function(colorArg){
+			for(var i=0;i<elementNodeList.length;i++){
+				this.elementNodeList.item(i).style.color=colorArg;
+				}
+		},
+		'text':function(textArg){
+			if(textArg){
+				this.elementNode.firstChild.nodeValue=textArg;
+			}else{
+				return this.elementNode.firstChild.nodeValue;
+			}
+		}
+		
+	}; 
+	return jQueryWrapperObject;
+};
+window.$=window.jQuery;
